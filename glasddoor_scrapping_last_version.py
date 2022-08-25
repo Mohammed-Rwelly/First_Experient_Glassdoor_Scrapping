54307d01da408226c1ad59156622100b1bc1108a
Mohammed Zwaib <46110270+Mohammed-Rwelly@users.noreply.github.com>
2022-08-25 14:30:01 -0700

    Update glasddoor_scrapping_last_version.py

--- glasddoor_scrapping_last_version.py
+++ glasddoor_scrapping_last_version.py
@@ -51,12 +51,12 @@ def get_jobs(keyword, num_jobs):
             except:
                 pass
             #after loding the page we are clicking on "see all the jobs " button 
-            time.sleep(2)
+            time.sleep(5)
             try:
                 driver.find_element(By.XPATH,'//span[@class="SVGInline css-1mgba7 css-1hjgaef"]').click()
             except:
                 pass
-            time.sleep(2) 
+            time.sleep(5) 
             
             while len(jobs_for_country) < num_jobs: 
                 job_buttons = driver.find_elements_by_xpath("//*[@id='MainCol']/div[1]/ul/li")
@@ -76,7 +76,7 @@ def get_jobs(keyword, num_jobs):
                         job_button.click()
                     except:
                         pass
-                    time.sleep(5)
+                    time.sleep(10)
                     try:
                         driver.find_element(By.XPATH,'//*[@id="JAModal"]/div/div[2]/span').click()
                     except NoSuchElementException:
