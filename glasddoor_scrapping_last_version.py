6eb1189724c7fe016473ff2ba969daae7bdb6518
Mohammed Zwaib <46110270+Mohammed-Rwelly@users.noreply.github.com>
2022-08-26 08:12:34 -0700

    Update glasddoor_scrapping_last_version.py

--- glasddoor_scrapping_last_version.py
+++ glasddoor_scrapping_last_version.py
@@ -57,7 +57,7 @@ def get_jobs(keyword, num_jobs):
             pass
         time.sleep(5)
         while len(jobs_for_country) < num_jobs: 
-           job_buttons = driver.find_elements_by_xpath("//*[@id='MainCol']/div[1]/ul/li")
+           job_buttons = driver.find_elements(By.XPATH,"//*[@id='MainCol']/div[1]/ul/li")
            # Going through each job url in this page
            job_buttons_href = driver.find_elements(By.XPATH,'//*[@id="MainCol"]/div[1]/ul/li/div[2]/a')
            #print("هون")
@@ -65,110 +65,114 @@ def get_jobs(keyword, num_jobs):
            print("Now we in {} ".format(number_of_all_page)) 
            #for job in range(len(job_buttons)):
            for job_button in job_buttons:
-               print("Progress: {}".format("" + str(len(jobs_for_country)) + "/" + str(num_jobs)))
-               if len(jobs_for_country) >= num_jobs:
-                   # When the number of jobs collected has reached the number we set. 
+               try:
+                   print("Progress: {}".format("" + str(len(jobs_for_country)) + "/" + str(num_jobs)))
+                   if len(jobs_for_country) >= num_jobs:
+                       # When the number of jobs collected has reached the number we set. 
+                       break
+                   try:
+                       job_button.click()
+                   except:
+                       pass
+                   time.sleep(4)
+                   try:
+                       driver.find_element(By.XPATH,'//*[@id="JAModal"]/div/div[2]/span').click()
+                   except NoSuchElementException:
+                       pass
+                   collected_successfully = False
+                                  
+                   while not collected_successfully:
+                       try:
+                           #time.sleep(10)
+                           job_num=job_buttons.index(job_button)
+                           company_name = driver.find_element(By.XPATH,'//div[@class="css-xuk5ye e1tk4kwz5"]').text
+                           location = driver.find_element(By.XPATH,'.//div[@class="css-56kyx5 e1tk4kwz1"]').text
+                           job_title = driver.find_element(By.XPATH,'.//div[@class="css-1j389vi e1tk4kwz2"]').text
+                           job_id  = job_buttons[job_num].get_attribute("data-id")
+                           job_url= job_buttons_href[job_num].get_attribute("href")
+                           #job_description = driver.find_element(By.XPATH,'.//div[@class="jobDescriptionContent desc"]').text
+                           collected_successfully = True
+                       except:
+                           collected_successfully = True
+              
+                   try:
+                       Posted_Date=driver.find_element(By.XPATH,'//*[@id="MainCol"]/div[1]/ul/li[{}]/div[2]/div[3]/div[2]/div[2]'.format(job_num+1)).text
+                   except NoSuchElementException:
+                       try:
+                           Posted_Date=driver.find_element(By.XPATH,'//*[@id="MainCol"]/div[1]/ul/li[{}]/div[2]/div[2]/div/div[2]'.format(job_num+1)).text
+                       except:
+                           Posted_Date="N/A"
+                   now = datetime.now()
+                   try:
+                       exdate= [int(x) for x in re.findall(r'-?\d+\.?\d*',Posted_Date)][0]
+                       Posted_Data_N=now.date() - timedelta(days=exdate)
+                   except:
+                       Posted_Data_N=now.date()
+                       
+                       
+                       
+                   #Click on "Show More" for extract full description                        
+                   try:
+                       driver.find_element(By.XPATH,'//div[@class="css-t3xrds e856ufb2"]').click()
+                       job_description_full = driver.find_element(By.XPATH,'.//div[@class="jobDescriptionContent desc"]').text
+                       #Extract job title from job description full 
+                       lines=job_description_full.splitlines()
+                       t=False
+                       try:
+                           for line in lines:
+                               if re.search('Job Type',line):
+                                   job_type = line.split(':')[1]
+                                   t=True
+                           if (t == False):
+                               job_type='N/A'
+                       except:
+                           job_type='N/A'      
+                   except NoSuchElementException:
+                       job_description_full ="N/A" 
+                       job_type='N/A'
+                       pass                       
+                   try:
+                       salary_estimate = driver.find_element(By.XPATH,'//*[@id="MainCol"]/div[1]/ul/li[{}]/div[2]/div[3]/div[1]/span'.format(job_num+1)).text
+                       
+                   except NoSuchElementException:
+                       # You need to set a "not found value. It's important."
+                       salary_estimate = 'N/A'
+                  # print("the salary is =",salary_estimate) 
+                   try: 
+                       rating = driver.find_element(By.XPATH,'//*[@id="employerStats"]/div[1]/div[1]').text
+                   except NoSuchElementException:
+                       # You need to set a "not found value. It's important."
+                       rating = 'N/A'
+                   #Extract Current Date Collection from a Datetime Object
+                   now = datetime.now()
+                   current_date = now.date()
+                   jobs_for_country.append({
+                   "Country" : country,
+                   "City" : location,
+                   "JobId" :job_id,
+                   "Source":"Glassdoor",
+                   "CollectedDate":current_date,
+                   "JobTitle" : job_title,
+                   "CompanyName" : company_name,
+                   "RatingNumber" : rating,
+                  "PostedDate":Posted_Date,
+                   "Salary" : salary_estimate,
+                   "jobType" :job_type,
+                   "JobURL" : job_url,
+                   "ShortDiscribtion" : "N/A",
+                   "fullJobDescribtion":job_description_full,
+                   "Posted_Date_N":Posted_Data_N,
+                   })                 
+               except:
+                   print("واطي ولاك")
                    break
-               try:
-                   job_button.click()
-               except:
-                   pass
-               time.sleep(10)
-               try:
-                   driver.find_element(By.XPATH,'//*[@id="JAModal"]/div/div[2]/span').click()
-               except NoSuchElementException:
-                   pass
-               collected_successfully = False
-                              
-               while not collected_successfully:
-                   try:
-                       #time.sleep(10)
-                       job_num=job_buttons.index(job_button)
-                       company_name = driver.find_element(By.XPATH,'//div[@class="css-xuk5ye e1tk4kwz5"]').text
-                       location = driver.find_element(By.XPATH,'.//div[@class="css-56kyx5 e1tk4kwz1"]').text
-                       job_title = driver.find_element(By.XPATH,'.//div[@class="css-1j389vi e1tk4kwz2"]').text
-                       job_id  = job_buttons[job_num].get_attribute("data-id")
-                       job_url= job_buttons_href[job_num].get_attribute("href")
-                       #job_description = driver.find_element(By.XPATH,'.//div[@class="jobDescriptionContent desc"]').text
-                       collected_successfully = True
-                   except:
-                       collected_successfully = True
-          
-               try:
-                   Posted_Date=driver.find_element(By.XPATH,'//*[@id="MainCol"]/div[1]/ul/li[{}]/div[2]/div[3]/div[2]/div[2]'.format(job_num+1)).text
-               except NoSuchElementException:
-                   try:
-                       Posted_Date=driver.find_element(By.XPATH,'//*[@id="MainCol"]/div[1]/ul/li[{}]/div[2]/div[2]/div/div[2]'.format(job_num+1)).text
-                   except:
-                       Posted_Date="N/A"
-               now = datetime.now()
-               try:
-                   exdate= [int(x) for x in re.findall(r'-?\d+\.?\d*',Posted_Date)][0]
-                   Posted_Data_N=now.date() - timedelta(days=exdate)
-               except:
-                   Posted_Data_N=now.date()
-                   
-                   
-                   
-               #Click on "Show More" for extract full description                        
-               try:
-                   driver.find_element(By.XPATH,'//div[@class="css-t3xrds e856ufb2"]').click()
-                   job_description_full = driver.find_element(By.XPATH,'.//div[@class="jobDescriptionContent desc"]').text
-                   #Extract job title from job description full 
-                   lines=job_description_full.splitlines()
-                   t=False
-                   try:
-                       for line in lines:
-                           if re.search('Job Type',line):
-                               job_type = line.split(':')[1]
-                               t=True
-                       if (t == False):
-                           job_type='N/A'
-                   except:
-                       job_type='N/A'      
-               except NoSuchElementException:
-                   job_description_full ="N/A" 
-                   job_type='N/A'
-                   pass                       
-               try:
-                   salary_estimate = driver.find_element(By.XPATH,'//*[@id="MainCol"]/div[1]/ul/li[{}]/div[2]/div[3]/div[1]/span'.format(job_num+1)).text
-                   
-               except NoSuchElementException:
-                   # You need to set a "not found value. It's important."
-                   salary_estimate = 'N/A'
-              # print("the salary is =",salary_estimate) 
-               try: 
-                   rating = driver.find_element(By.XPATH,'//*[@id="employerStats"]/div[1]/div[1]').text
-               except NoSuchElementException:
-                   # You need to set a "not found value. It's important."
-                   rating = 'N/A'
-               #Extract Current Date Collection from a Datetime Object
-               now = datetime.now()
-               current_date = now.date()
-               jobs_for_country.append({
-               "Country" : country,
-               "City" : location,
-               "JobId" :job_id,
-               "Source":"Glassdoor",
-               "CollectedDate":current_date,
-               "JobTitle" : job_title,
-               "CompanyName" : company_name,
-               "RatingNumber" : rating,
-              "PostedDate":Posted_Date,
-               "Salary" : salary_estimate,
-               "jobType" :job_type,
-               "JobURL" : job_url,
-               "ShortDiscribtion" : "N/A",
-               "fullJobDescribtion":job_description_full,
-               "Posted_Date_N":Posted_Data_N,
-               })
            # Clicking on the "next page" button
            try:
                page = driver.find_element(By.XPATH,'//*[@id="MainCol"]/div[2]/div/div[2]').text
                page = page.split()
                if page[1]==page[3]:
                    break
-               driver.find_element_by_css_selector('[alt="next-icon"]').click()
+               driver.find_element(By.CSS_SELECTOR,'[alt="next-icon"]').click()
                time.sleep(5)
            except:
                print("Scraping terminated before reaching target number of jobs. Needed {}, got {}.".format(num_jobs, len(jobs_for_country)))
