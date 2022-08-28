890a4da82115ed393187b3e38391c5d9ca38fef7
Mohammed Zwaib <46110270+Mohammed-Rwelly@users.noreply.github.com>
2022-08-28 00:47:13 -0700

    Update glasddoor_scrapping_last_version.py

--- glasddoor_scrapping_last_version.py
+++ glasddoor_scrapping_last_version.py
@@ -9,8 +9,8 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from datetime import datetime,timedelta
 import re
-locations = ["Istanbul"]
-#locations = ["Abu Dhabi","Istanbul","Ankara","İzmir","Barcelona","Valencia","Riyadh","Muscat","Tokyo","Rome"]
+#locations = ["Istanbul"]
+locations = ["Abu Dhabi","Istanbul","Ankara","İzmir","Barcelona","Valencia","Riyadh","Muscat","Tokyo","Rome"]
 #locations = ["Qatar","United Kingdom","Teresina","João Pessoa","Aracaju","Berlin","Hamburg","Munich","Szolnok","Sopron","Stans","Dubai","France"]
 #locations = ["France","Qatar"]
 def get_jobs(keyword, num_jobs):
@@ -170,7 +170,7 @@ def get_jobs(keyword, num_jobs):
            try:
                page = driver.find_element(By.XPATH,'//*[@id="MainCol"]/div[2]/div/div[2]').text
                page = page.split()
-               if page[1]==page[3] or page[1]> page[3]:
+               if page[1]==page[3]:
                    print("hhhhh")
                    break
                driver.find_element(By.CSS_SELECTOR,'[alt="next-icon"]').click()
